7d93b838853d2fc5a23969a81b2da35686c70ae1
Adam Porter <adam@alphapapa.net>
2020-11-24 06:43:39 -0600

    argh

--- org-ql.el
+++ org-ql.el
@@ -722,8 +722,8 @@ Arguments STRING, POS, FILL, and LEVEL are according to
   Builds the PEG expression using PREDICATES (which should be the
   value of `org-ql-predicates')."
     ;; HACK:
-    (message "LOADING PEG")
-    (require 'peg)
+    (message "LOADING PEG:%S" (require 'peg))
+
     (message "LOADED PEG")
     (let* ((names (--map (symbol-name (plist-get (cdr it) :name))
                          predicates))
