aa4b393165d0d9d202c92b658a534c2b4e23c8f2
Adam Porter <adam@alphapapa.net>
2020-11-24 06:24:30 -0600

    WIP: peg stuff

--- org-ql.el
+++ org-ql.el
@@ -670,35 +670,50 @@ Arguments STRING, POS, FILL, and LEVEL are according to
 ;; This section implements parsing of "plain," non-Lisp queries using the `peg'
 ;; library.  NOTE: This needs to appear after the predicates are defined.
 
-(require 'peg)
-
 (eval-and-compile
   ;; This `eval-when' is necessary, otherwise the macro does not define
   ;; the function correctly, apparently because `org-ql-predicates'
   ;; ends up being not defined correctly at expansion time.
 
+  (require 'peg)
   ;; Fix compiler warnings probably caused by `peg' not using lexical-binding.
   ;; TODO: File bug report upstream.
   (defvar peg-errors nil)
   (defvar peg-stack nil)
 
-  (defmacro org-ql--peg-parse-string (rules string &optional noerror)
-    "Parse STRING according to RULES."
-    ;; This sentence was in the docstring but Checkdoc is complaining,
-    ;; so moving it to a comment: "If NOERROR is non-nil, push nil
-    ;; resp. t if the parse failed resp. succeded instead of signaling
-    ;; an error."
-
-    ;; Unfortunately, this macro was moved to peg-tests.el, so we copy it here.
-    `(with-temp-buffer
-       (insert ,string)
-       (goto-char (point-min))
-       ,(if noerror
-            (let ((entry (gensym "entry"))
-                  (start (caar rules)))
-              `(peg-parse (,entry (or (and ,start `(-- t)) ""))
-                          . ,rules))
-          `(peg-parse . ,rules))))
+  ;; (defmacro argh-org-ql--peg-parse-string (rules string &optional noerror)
+  ;;   "Parse STRING according to RULES."
+  ;;   ;; This sentence was in the docstring but Checkdoc is complaining,
+  ;;   ;; so moving it to a comment: "If NOERROR is non-nil, push nil
+  ;;   ;; resp. t if the parse failed resp. succeded instead of signaling
+  ;;   ;; an error."
+  ;;
+  ;;   ;; Unfortunately, this macro was moved to peg-tests.el, so we copy it here.
+  ;;   `(with-temp-buffer
+  ;;      (insert ,string)
+  ;;      (goto-char (point-min))
+  ;;      ,(if noerror
+  ;;           (let ((entry (gensym "entry"))
+  ;;                 (start (caar rules)))
+  ;;             `(peg-parse (,entry (or (and ,start `(-- t)) ""))
+  ;;                         . ,rules))
+  ;;         `(peg-parse . ,rules))))
+  ;;
+  ;; (defun org-ql--peg-parse-string (rules string &optional noerror)
+  ;;   "Parse STRING according to RULES."
+  ;;   ;; This sentence was in the docstring but Checkdoc is complaining,
+  ;;   ;; so moving it to a comment: "If NOERROR is non-nil, push nil
+  ;;   ;; resp. t if the parse failed resp. succeded instead of signaling
+  ;;   ;; an error."
+  ;;
+  ;;   ;; Unfortunately, this macro was moved to peg-tests.el, so we copy it here.
+  ;;   (with-temp-buffer
+  ;;     (insert string)
+  ;;     (goto-char (point-min))
+  ;;     (let ((entry (make-symbol "entry"))
+  ;;           (start (caar rules)))
+  ;;       (eval (peg-translate-rules `( (,entry (or (and ,start `(-- t)) "")) ,@rules)
+  ;;                                  )))))
 
   (defun org-ql--def-query-string-to-sexp-fn (predicates)
     "Define function `org-ql--query-string-to-sexp' according to PREDICATES.
@@ -716,42 +731,50 @@ Arguments STRING, POS, FILL, and LEVEL are according to
                             ;; Sort the keywords longest-first to work around what seems to be an
                             ;; obscure bug in `peg': when one keyword is a substring of another,
                             ;; and the shorter one is listed first, the shorter one fails to match.
-                            (-sort (-on #'> #'length)))))
-      (fset 'org-ql--query-string-to-sexp
-            (byte-compile
-             `(lambda (input &optional boolean)
-                "Return query parsed from plain query string INPUT.
+                            (-sort (-on #'> #'length))))
+           (peg-rules `((query (+ term
+                                  (opt (+ (syntax-class whitespace) (any)))))
+                        (term (or (and negation (list positive-term)
+                                       ;; This is a bit confusing, but it seems to work.  There's probably a better way.
+                                       `(pred -- (list 'not (car pred))))
+                                  positive-term))
+                        (positive-term (or (and predicate-with-args `(pred args -- (cons (intern pred) args)))
+                                           (and predicate-without-args `(pred -- (list (intern pred))))
+                                           (and plain-string `(s -- (list 'regexp s)))))
+                        (plain-string (or quoted-arg unquoted-arg))
+                        (predicate-with-args (substring predicate) ":" args)
+                        (predicate-without-args (substring predicate) ":")
+                        (predicate (or ,@predicates))
+                        (args (list (+ (and (or keyword-arg quoted-arg unquoted-arg) (opt separator)))))
+                        (keyword-arg (and keyword "=" `(kw -- (intern (concat ":" kw)))))
+                        (keyword (substring (+ (not (or separator "=" "\"" (syntax-class whitespace))) (any))))
+                        (quoted-arg "\"" (substring (+ (not (or separator "\"")) (any))) "\"")
+                        (unquoted-arg (substring (+ (not (or separator "\"" (syntax-class whitespace))) (any))))
+                        (negation "!")
+                        (separator "," )))
+           (parser-form
+            ;; Imitating the macro `peg-parse'.
+            (let ((entry (make-symbol "entry"))
+                  (start (caar peg-rules)))
+              (peg-translate-rules `( (,entry (or (and ,start `(-- t)) "")) ,@peg-rules))))
+           (lambda `(lambda (input &optional boolean)
+                      "Return query parsed from plain query string INPUT.
   Multiple predicates are combined with BOOLEAN (default: `and')."
-                (unless (s-blank-str? input)
-                  (let* ((boolean (or boolean 'and))
-                         (query (org-ql--peg-parse-string
-                                 ((query (+ term
-                                            (opt (+ (syntax-class whitespace) (any)))))
-                                  (term (or (and negation (list positive-term)
-                                                 ;; This is a bit confusing, but it seems to work.  There's probably a better way.
-                                                 `(pred -- (list 'not (car pred))))
-                                            positive-term))
-                                  (positive-term (or (and predicate-with-args `(pred args -- (cons (intern pred) args)))
-                                                     (and predicate-without-args `(pred -- (list (intern pred))))
-                                                     (and plain-string `(s -- (list 'regexp s)))))
-                                  (plain-string (or quoted-arg unquoted-arg))
-                                  (predicate-with-args (substring predicate) ":" args)
-                                  (predicate-without-args (substring predicate) ":")
-                                  (predicate (or ,@predicates))
-                                  (args (list (+ (and (or keyword-arg quoted-arg unquoted-arg) (opt separator)))))
-                                  (keyword-arg (and keyword "=" `(kw -- (intern (concat ":" kw)))))
-                                  (keyword (substring (+ (not (or separator "=" "\"" (syntax-class whitespace))) (any))))
-                                  (quoted-arg "\"" (substring (+ (not (or separator "\"")) (any))) "\"")
-                                  (unquoted-arg (substring (+ (not (or separator "\"" (syntax-class whitespace))) (any))))
-                                  (negation "!")
-                                  (separator "," ))
-                                 input 'noerror)))
-                    ;; Discard the t that `peg-parse-string' always returns as the first
-                    ;; element.  I don't know what it means, but we don't want it.
-                    (if (> (length (cdr query)) 1)
-                        (cons boolean (nreverse (cdr query)))
-                      (cadr query)))))))))
-  )
+                      (unless (s-blank-str? input)
+                        (let* ((boolean (or boolean 'and))
+                               (query-sexp
+                                (with-temp-buffer
+                                  (insert input)
+                                  (goto-char (point-min))
+                                  ,parser-form)))
+                          ;; Discard the t that `peg-parse-string' always returns as the first
+                          ;; element.  I don't know what it means, but we don't want it.
+                          (if (> (length (cdr query-sexp)) 1)
+                              (cons boolean (nreverse (cdr query-sexp)))
+                            (cadr query-sexp)))))))
+      ;; Byte-compile after the function is defined to avoid lexical variable warnings.
+      (fset 'org-ql--query-string-to-sexp (byte-compile lambda))
+      )))
 
 ;;;;; Predicate definition
 
